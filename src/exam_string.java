6baa4dbe6773688193da6dc58210c16e981492a6
dazzlingtea <kitsnr08@gmail.com>
2024-02-08 10:08:24 +0900

    Integer.parseInt 를 사용해 문장 정수화

--- src/exam_string.java
+++ src/exam_string.java
@@ -8,11 +8,36 @@ public class exam_string {
 		
 		// 향상된 for문
 		for(String bit : queryStringBits) {
-			System.out.println(bit);
+		System.out.println(bit);
 		}
 		
 		// stream 
 		Arrays.stream(queryStringBits)
 			.forEach(System.out::println);
+		
+		// 파라미터 이름/ 파라미터 값
+		int a=0, b=0, c=0;
+		Integer d = null; // 객체 타입 변수는 null 가능
+		
+		for(String bit : queryStringBits) {
+			String[] bitBits = bit.split("=");
+			String paraName = bitBits[0];
+			String paraValue = bitBits[1];
+			
+			System.out.printf("%s : %s\n", paraName, paraValue);
+			
+			if(paraName.equals("a")) {
+				a = Integer.parseInt(paraValue);
+			}
+			else if(paraName.equals("b")) {
+				b = Integer.parseInt(paraValue);
+			}
+			else if(paraName.equals("c")) {
+				c = Integer.parseInt(paraValue);
+			}
+		}
+		System.out.printf("a : %s\n", a);
+		System.out.printf("b : %s\n", b);
+		System.out.printf("c : %s\n", c);
 	}
 }
