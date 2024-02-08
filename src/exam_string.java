2755a34f3caa89472d401e325a2963ad83b95fae
dazzlingtea <kitsnr08@gmail.com>
2024-02-08 10:23:21 +0900

    리스트2개로 파라미터 이름, 값 담기

--- src/exam_string.java
+++ src/exam_string.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 //query string 요청에 맞게 데이터를 처리하는 방법
 public class exam_string {
@@ -39,5 +41,28 @@ public class exam_string {
 		System.out.printf("a : %s\n", a);
 		System.out.printf("b : %s\n", b);
 		System.out.printf("c : %s\n", c);
+		
+		System.out.println("===============");
+		
+		// a=100&b....&d=10 -> d는 출력 불가, 즉 추가할 때마다 문제가 생김
+		// a,b,c ... 리스트1, 100,20,30,... 값 리스트2 -> 사실 비효율 -> Map 필요
+		List<String> paramNames = new ArrayList<>();
+		List<Integer> paramValues = new ArrayList<>();
+				
+		for(String bit : queryStringBits) {
+			String[] bitBits = bit.split("=");
+			String paraName = bitBits[0];
+			String paraValue = bitBits[1];
+			
+			paramNames.add(paraName);
+			paramValues.add(Integer.parseInt(paraValue));
+		}
+		
+		for(int i=0; i < paramNames.size(); i++) {
+			String paramName = paramNames.get(i);
+			int paramValue = paramValues.get(i);
+			
+			System.out.printf("%s : %d\n", paramName, paramValue);
+		}
 	}
 }
